052342b496455b5c1cc5256a8d0bc56a5e59e56b
pdxalz <pdxalz@users.noreply.github.com>
2021-03-28 09:50:58 -0700

    Merged in level switch model code
    Level switch mode was modified version of the on/off mesh sample.
    Also changed the device name to GUS_Teacher.

--- src/main.c
+++ src/main.c
@@ -17,35 +17,43 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-static void on_app_ble_event(app_ble_event_data_t * evt_data)
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh/models.h>
+#include <bluetooth/mesh/dk_prov.h>
+#include <dk_buttons_and_leds.h>
+#include "model_handler.h"
+
+static void bt_ready(int err)
 {
-	switch(evt_data->type) {
-		case APP_BLE_CONNECTED:
-			printk("Connected\n");
-			gui_set_bt_state(GUI_BT_STATE_CONNECTED);
-			break;
-
-		case APP_BLE_DISCONNECTED:
-			printk("Disconnected\n");
-			gui_set_bt_state(GUI_BT_STATE_ADVERTISING);
-
-			app_ble_start_advertising();
-			break;
-
-		case APP_BLE_ON_LED_WRITE:
-			printk("Led %s\n", evt_data->led_state ? "On" : "Off");
-			gui_set_bt_led_state(evt_data->led_state);
-			break;
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
 	}
+
+	printk("Bluetooth initialized\n");
+
+	dk_leds_init();
+	dk_buttons_init(NULL);
+
+	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
+	if (err) {
+		printk("Initializing mesh failed (err %d)\n", err);
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	/* This will be a no-op if settings_load() loaded provisioning info */
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+
+	printk("Mesh initialized\n");
 }
 
 void on_gui_event(gui_event_t *event)
 {
-	switch(event->evt_type) {
-		case GUI_EVT_BUTTON_PRESSED:
-			app_ble_send_button_state(event->button_checked);
-			break;
-	}
+
 }
 
 void main(void)
@@ -53,14 +61,14 @@ void main(void)
 	gui_config_t gui_config = {.event_callback = on_gui_event};
 	gui_init(&gui_config);
 
-	//app_ble_config_t app_ble_config = {.event_callback = on_app_ble_event};
-	//app_ble_init(&app_ble_config);
+	int err;
 
-	//app_ble_start_advertising();
+	printk("Initializing...\n");
 
-	//printk("Advertising started\n");
-
-	//gui_set_bt_state(GUI_BT_STATE_ADVERTISING);
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
 
 	while (1) {
 		k_sleep(K_MSEC(1000));
